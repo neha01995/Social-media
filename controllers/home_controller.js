5d8fb91de9685de6e2d71f9458870c7cb060d27c
Neha singla <nehasingla7180@gmail.com>
2020-05-03 18:14:59 +0530

    Added async await conditions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,57 @@
 const Post=require('../models/post');
 const User=require('../models/user');
 
-module.exports.home=function(req,res){
 
-    // Post.find({ }, function(err,posts){
-    //     return res.render('home',{
-    //         title:'Home',
-    //         posts:posts
+module.exports.home=async function(req,res){
 
-    // });
+    try{
+            let posts=await Post.find({ })
+            .populate('user')
+            .populate({
+                path:'comments',
+                populate :{
+                    path:'user'
+                }
+            });
+               let users=await User.find({});
 
-    Post.find({ })
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate :{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({} ,function(err,user){
-            return res.render('home',{
-                title:'Home',
-                posts:posts,
-                all_users:user
+                    return res.render('home',{
+                        title:'Home',
+                        posts:posts,
+                        all_users:users
+        
+            });
+        
 
-        })
-
-
-
-    });
-
-    });
-
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
+
+// module.exports.home=function(req,res){
+
+//     Post.find({ })
+//     .populate('user')
+//     .populate({
+//         path:'comments',
+//         populate :{
+//             path:'user'
+//         }
+//     })
+//     .exec(function(err,posts){
+//         User.find({} ,function(err,user){
+//             return res.render('home',{
+//                 title:'Home',
+//                 posts:posts,
+//                 all_users:user
+
+//         })
+
+
+
+//     });
+
+//     });
+
+// }
